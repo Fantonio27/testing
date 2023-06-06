9b262b46301a9f2b56559649878ac7d8da86a623
Francis Louie G. Antonio <francis.antonio0927@gmail.com>
2023-06-06 22:40:16 +0800

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+    const express = require('express');
+    const cors = require('cors');
+    const mongoose = require('mongoose');
 
-const HealthRouter = require('./routes/health');
-require('dotenv').config();
+    const HealthRouter = require('./routes/health');
+    require('dotenv').config();
 
-const app = express();
-const port = process.env.PORT || 5000;
+    const app = express();
+    const port = process.env.PORT || 5000;
 
-//middleware
-app.use(cors());
-app.use(express.json());
+    //middleware
+    app.use(cors());
+    app.use(express.json());
 
 
-//mongodb connection
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+    //mongodb connection
+    const uri = process.env.ATLAS_URI;
+    mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
-const connection = mongoose.connection;
-connection.once('open', ()=>{
-    console.log("MongoDB database connection is established.");
-});
+    const connection = mongoose.connection;
+    connection.once('open', ()=>{
+        console.log("MongoDB database connection is established.");
+    });
 
-app.use('https://frolicking-wisp-a9d2c7.netlify.app/health', HealthRouter);
+    app.use('/health', HealthRouter);
 
-app.listen(port, ()=> {
-    console.log(`Server is running in port: ${port}`);
-})
+    app.listen(port, ()=> {
+        console.log(`Server is running in port: ${port}`);
+    })
 
